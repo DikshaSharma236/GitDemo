996d3b696c3cae88463a3c6526ec7512b42dd837
Vishu <Vishu@Parivesh>
2021-04-05 16:07:40 +0530

    editing..parivesh

--- introduction/src/introduction/Dikshaji.java
+++ introduction/src/introduction/Dikshaji.java
@@ -2,13 +2,19 @@ package introduction;
 
 import java.sql.Driver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Dikshaji {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		
-		System.setProperty("webdriver.chrome.driver", "gdgagdadhaggdg");
+		System.setProperty("webdriver.chrome.driver", "C:\\work\\chromedriver.exe");
+		WebDriver  driver=new ChromeDriver();
+		driver.get("parivesh.com");
+		
 
 	}
 
